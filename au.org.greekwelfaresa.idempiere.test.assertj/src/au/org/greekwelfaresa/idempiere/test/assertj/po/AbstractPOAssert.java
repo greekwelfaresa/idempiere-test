7870cec87cef10ee221224c4195d84e830e95658
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2025-06-18 22:01:57 +0930

    [assertj] Added attachment assertions to AbstractPOAssert

--- au.org.greekwelfaresa.idempiere.test.assertj/src/au/org/greekwelfaresa/idempiere/test/assertj/po/AbstractPOAssert.java
+++ au.org.greekwelfaresa.idempiere.test.assertj/src/au/org/greekwelfaresa/idempiere/test/assertj/po/AbstractPOAssert.java
@@ -2,10 +2,13 @@ package au.org.greekwelfaresa.idempiere.test.assertj.po;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
 import java.util.Properties;
 
+import org.compiere.model.MAttachment;
+import org.compiere.model.MAttachmentEntry;
 import org.compiere.model.PO;
 
 import au.org.greekwelfaresa.idempiere.test.assertj.AbstractIDAssert;
@@ -28,6 +31,60 @@ public abstract class AbstractPOAssert<SELF extends AbstractPOAssert<SELF, ACTUA
 	protected String getPODescription() {
 		return getPODescription(actual);
 	}
+
+	public SELF hasAttachments() {
+		isNotNull();
+		MAttachment a = MAttachment.get(actual.getCtx(), actual.get_Table_ID(), actual.get_ID(), actual.get_TrxName());
+		if (a == null || a.getEntryCount() == 0) {
+			failWithMessage("\nExpecting PO:\n <%s>\nto have attachments\nbut it doesn't", getPODescription());
+		}
+		return myself;
+	}
+	
+	public SELF hasAttachmentEntryCount(int expected) {
+		isNotNull();
+		MAttachment a = MAttachment.get(actual.getCtx(), actual.get_Table_ID(), actual.get_ID(), actual.get_TrxName());
+		final int actual = a == null ? 0 : a.getEntryCount();
+		if (actual != expected) {
+			failWithActualExpectedAndMessage(actual, expected, "\nExpecting PO:\n <%s>\nto have <%d> attachments\nbut it has <%d>", getPODescription(), expected, actual);
+		}
+		return myself;
+	}
+	
+	public SELF hasAttachmentEntry(String name, byte[] expected) {
+		isNotNull();
+		System.err.println("Looking for actual: " + actual.get_Table_ID() + ", " + actual.get_ID());
+		MAttachment a = MAttachment.get(actual.getCtx(), actual.get_Table_ID(), actual.get_ID(), actual.get_TrxName());
+		System.err.println("Looking for actual attachment: " + a + ", " + (a != null ? a.getEntryCount() : " null"));
+		if (a == null || a.getEntryCount() == 0) {
+			failWithMessage("\nExpecting PO:\n <%s>\nto have attachments\nbut it doesn't", getPODescription());
+		}
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < a.getEntryCount(); i++) {
+			final MAttachmentEntry e = a.getEntry(i);
+			b.append("\n  ").append(e.getName());
+			if (Objects.equals(e.getName(), name)) {
+				final byte[] actual = e.getData();
+				if (!Arrays.equals(expected, actual)) {
+					final String expectedString = Arrays.toString(expected);
+					final String actualString = Arrays.toString(actual);
+					failWithActualExpectedAndMessage(actual, expected, "\nExpecting PO:\n  <%s>\nto have entry <%s>\nwith data\n <%s>\nbut entry has data\n <%s>", getPODescription(), name, expectedString, actualString);
+				}
+				return myself;
+			}
+		}
+		failWithMessage("\nExpecting PO:\n <%s>\nto have attachment entry\n <%s>\nbut there was no entry with that name.\nThe following entries were present:%s", getPODescription(), name, b.toString());
+		return myself;
+	}
+	
+	public SELF doesNotHaveAttachments() {
+		isNotNull();
+		MAttachment a = MAttachment.get(actual.getCtx(), actual.get_Table_ID(), actual.get_ID(), actual.get_TrxName());
+		if (a != null && a.getEntryCount() != 0) {
+			failWithMessage("\nExpecting PO:\n <%s>\nto have no attachments\nbut it has <%d>", getPODescription(), a.getEntryCount());
+		}
+		return myself;
+	}
 	
 	public SELF hasColumnCount(int expected) {
 		isNotNull();
@@ -38,7 +95,25 @@ public abstract class AbstractPOAssert<SELF extends AbstractPOAssert<SELF, ACTUA
 		return myself;
 	}
 	
-	public SELF hasColumn(String columnName, Object expected) {
+	public SELF hasColumn(String columnName) {
+		isNotNull();
+		
+		if (!actual.columnExists(columnName)) {
+			failWithMessage("\nExpecting PO:\n <%s>\nto have column <%s>\nbut it does not", getPODescription());
+		}
+		return myself;
+	}
+	
+	public SELF doesNotHaveColumn(String columnName) {
+		isNotNull();
+		
+		if (actual.columnExists(columnName)) {
+			failWithMessage("\nExpecting PO:\n <%s>\nto not have column <%s>\nbut it does", getPODescription());
+		}
+		return myself;
+	}
+	
+	public SELF hasValue(String columnName, Object expected) {
 		isNotNull();
 		
 		Object actualColumn = actual.get_Value(columnName);
@@ -247,7 +322,7 @@ public abstract class AbstractPOAssert<SELF extends AbstractPOAssert<SELF, ACTUA
 		isNotNull();
 		Timestamp a = actual.getCreated();
 		if (!Objects.equals(expected, a)) {
-			failWithMessage("\nExpecting PO:\n  <%s>\nto have been updated at: <%s>\nbut it was updated at: <%s>",
+			failWithMessage("\nExpecting PO:\n  <%s>\nto have been created at: <%s>\nbut it was created at: <%s>",
 					getPODescription(), expected == null ? null : new Timestamp(expected.getTime()), a);
 		}
 		return myself;
